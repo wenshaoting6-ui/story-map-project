0d790b2862e166aee386ae4db023f253def06a20
wenshaoting6-ui <wenshaoting6@gmail.com>
2026-09-16 15:38:34 -0400

    draft

--- templates/scrollytelly/js/index.js
+++ templates/scrollytelly/js/index.js
@@ -3,9 +3,9 @@ import { SlideDeck } from './slidedeck.js';
 const map = L.map('map', {scrollWheelZoom: false}).setView([0, 0], 0);
 
 // ## The Base Tile Layer
-const baseTileLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
-  maxZoom: 16,
-  attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+const baseTileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=cb1_3nr5_1_a495e7b51cea217486734425', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 });
 baseTileLayer.addTo(map);
 
@@ -17,8 +17,8 @@ const slideOptions = {
   'second-slide': {
     style: (feature) => {
       return {
-        color: 'red',
-        fillColor: 'green',
+        color: '#722F37',
+        fillColor: '#9B2226',
         fillOpacity: 0.5,
       };
     },
@@ -26,11 +26,90 @@ const slideOptions = {
       layer.bindTooltip(feature.properties.label);
     },
   },
+  
   'third-slide': {
     style: (feature) => {
       return {
-        color: 'blue',
-        fillColor: 'yellow',
+        color: '#722F37',
+        fillColor: '#9B2226',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+
+  'forth-slide': {
+    style: (feature) => {
+      return {
+        color: '#B8860B',
+        fillColor: '#F3E5AB',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+
+  'fifth-slide': {
+    style: (feature) => {
+      return {
+        color: '#722F37',
+        fillColor: '#9B2226',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+  
+  'sixth-slide': {
+    style: (feature) => {
+      return {
+        color: '#C5A059',
+        fillColor: '#FFFDD0',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+
+  'seventh-slide': {
+    style: (feature) => {
+      return {
+        color: '#C5A059',
+        fillColor: '#FFFDD0',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+
+  'eighth-slide': {
+    style: (feature) => {
+      return {
+        color: '#2D6A4F',
+        fillColor: '#74C69D',
+        fillOpacity: 0.5,
+      };
+    },
+    onEachFeature: (feature, layer) => {
+      layer.bindTooltip(feature.properties.label);
+    },
+  },
+
+  'ninth-slide': {
+    style: (feature) => {
+      return {
+        color: '#2D6A4F',
+        fillColor: '#74C69D',
         fillOpacity: 0.5,
       };
     },
